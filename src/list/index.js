fd1bd0a568f27ba5fc7aa2b3d01d085fccc9c031
UdaySreddy <103050184+UdaySreddy@users.noreply.github.com>
2022-05-07 22:05:21 +0530

    Update index.js
    all cases passed

--- src/list/index.js
+++ src/list/index.js
@@ -1,22 +1,31 @@
 import './index.css'
 
 const List = props => {
-  const {siteList} = props
-  const {timeAccessed, logoUrl, title, domainUrl} = siteList
+  const {siteList, deleteSearch} = props
+  const {timeAccessed, logoUrl, title, domainUrl, id} = siteList
   console.log(timeAccessed)
+  const onDelete = () => {
+    deleteSearch(id)
+  }
+
   return (
     <li className="listMain">
       <div className="list1">
         <p className="p1">{timeAccessed}</p>
-        <img className="image1" src={logoUrl} alt="img" />
+        <img className="image1" src={logoUrl} alt="domain logo" />
         <p className="p2">{title}</p>
         <p className="p3">{domainUrl}</p>
       </div>
       <div>
-        <button type="button" className="image1">
+        <button
+          type="button"
+          testid="delete"
+          className="image1"
+          onClick={onDelete}
+        >
           <img
             src="https://assets.ccbp.in/frontend/react-js/delete-img.png "
-            alt="X"
+            alt="delete"
           />
         </button>
       </div>
